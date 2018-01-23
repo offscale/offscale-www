8bfd2f9ee01f21ca395a0c724072d38aada18296
Samuel Marks <samuelmarks@gmail.com>
2018-01-23 19:14:09 +1100

    Wrote getting-started guide; replace footer /in/ with logo; removed unneeded mod from scaffold; updated README.md; upgraded dependencies

--- src/app/learn-engineering/learn-engineering.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Routes } from '@angular/router';
-
-import { LearnEngineeringComponent } from './learn-engineering.component';
-
-export const learnEngineeringRoutes: Routes = [
-  {path: '', component: LearnEngineeringComponent}
-];
